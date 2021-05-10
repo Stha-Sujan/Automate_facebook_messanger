64acfc38e48aa189284d339d862418cc07193dad
Stha-Sujan <mesujanstha143@gmail.com>
2021-05-10 14:29:12 +0545

    this is first

---
+++ main.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+while True:
+    time.sleep(3)
+    pyautogui.typewrite('Please allow me, I am a python scripted automate program ')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.typewrite('I am just a automated chat program of Sujan Shrestha.')
+    time.sleep(4)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.typewrite('This is just a test done on this chat group.')
+    time.sleep(4)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.typewrite('Hope you guys are doing good....')
+    time.sleep(4)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.typewrite('Thank you!! .... Have a nice day.')
+    time.sleep(4)
+    pyautogui.press('enter')
